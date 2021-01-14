b8eb73282333d5f890d97d978a2e60867ec30f12
kpmrozowski <kpmrozowski@gmail.com>
2021-01-14 12:43:22 +0000

    1i2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,4 +1,5 @@
 #include "catch.hpp"
+using namespace std;
 #include<iostream>
 
 // Zad2
@@ -6,41 +7,24 @@
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 class Makaron
 {
-public:
-    Makaron() = default;
-    ~Makaron() = default;
-
-    virtual double ileMaki(unsigned) const = 0;
-
-    static Makaron* gotujMakaron(std::string&);
+  public:
+  virtual double ileMaki (unsigned P) const =0;
+  static Makaron* gotujMakaron(const string&);
+  ~Makaron(){};
 };
-
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
-class Tagliatelle : public Makaron {
-private:
-    double L;
-    double W;
-    double R;
 
-    static const double C;
-
-public:
-		Tagliatelle(double a, double b, double c) :L(a), W(b), R(c) {}
-    Tagliatelle() {
-			L = 0.5;
-			W = 0.5;
-			R = 0.5;
-		}
-    ~Tagliatelle() {}
-		
-    double ileMaki(unsigned P) const override;
+class Tagliatelle :public Makaron
+{
+  private:
+  double L;
+  double W;
+  double R;
+  static const double C;
+  public:
+  Tagliatelle(double a, double b, double c) : L(a), W(b),R(c){};
+  Tagliatelle() {L=0.5;W=0.5;R=0.5;};
+  double ileMaki(unsigned P) const {return P*L*W*(1-R)*C;};
 };
-
-double Tagliatelle::ileMaki(unsigned P) const {
-    return P * L * W *(1. - R) * C;
-}
-
-
-
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -1,19 +1,18 @@
 #include "zad1.hpp"
-
 #include <memory>
 #include <string>
 #include <type_traits>
 
 // tutaj klasa Penne
 
+// tutaj definicja metody gotujMakaron
+
 class Penne : public Makaron {
   public:
   double ileMaki(unsigned P) const {return(double) P;};
 };
-
-// tutaj definicja metody gotujMakaron
-
-Makaron* Makaron::gotujMakaron(const string& makaron) {
+Makaron* Makaron::gotujMakaron(const string& makaron)
+{
     if (makaron.front() == makaron.back())
         return new Tagliatelle(3.14, 0.42, 0.1);
     else 
