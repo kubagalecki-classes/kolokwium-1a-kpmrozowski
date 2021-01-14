c50eaf8e71c1d10af0a1919f6ac0ab8a68118a7a
kpmrozowski <kpmrozowski@gmail.com>
2021-01-14 11:46:45 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -14,9 +14,7 @@ class Tagliatelle {
  static const double C;
 
  public:
- Tagliatelle(double a, double b, double c) : L{a}, W{b}, R{C} {
-	 ;
- }
+ Tagliatelle(double a, double b, double c) : L{a}, W{b}, R{C} {}
  Tagliatelle() : L{0.5}, W{0.5}, R{0.5} {}
  double ileMaki(unsigned P) const {
 	 return P * L * W * (1. - R) * C;
