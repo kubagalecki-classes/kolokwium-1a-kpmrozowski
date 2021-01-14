a6e664e4b538511b7a7c1eee8dddd7d58afbe9bf
kpmrozowski <kpmrozowski@gmail.com>
2021-01-14 12:37:13 +0000

    zad1,2

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -19,34 +19,3 @@ Makaron* Makaron::gotujMakaron(const string& makaron) {
     else 
         return new Penne;
 };
-
-
-
-
-#include "zad1.hpp"
-
-#include <memory>
-#include <string>
-#include <type_traits>
-
-// tutaj klasa Penne
-
-// tutaj definicja metody gotujMakaron
-
-class Penne : public Makaron
-{
-  public:
-  double ileMaki(unsigned P) const {return(double) P;};
-};
-
-Makaron* Makaron::gotujMakaron(const string& makaron)
-{
-   
-    if (makaron.front() == makaron.back())
-    
-        return new Tagliatelle(3.14, 0.42, 0.1);
-   
-    else 
-        return new Penne;
-
-};
